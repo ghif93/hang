c254a8335591a309a9903a60099fc44a46f6dfd0
unknown <Administrator@ACS.CORP>
2023-08-24 19:48:53 -0300

    fix

--- hangman.py
+++ hangman.py
@@ -12,20 +12,30 @@ for x in range(len(chosen_word)):
 
 end_of_game=False
 
-life=5
+life=0
 
 while not end_of_game:
-    guess = input("Guess the letter ").lower
-
+    
+    guess = input("Guess the letter ").lower()
     for num in range(len(chosen_word)):
             if(chosen_word[num] == guess):
                 display[num]=guess
-            else:
             
-                life-=1
     
     print(display)
+    if(guess not in display):
+        life+=1
+        if(life==5):
+            end_of_game=True
+            print("You lost")
+    
 
+
+
+
+    if('_' not in display):
+         end_of_game=True
+         print("You win")
 
 
 
@@ -35,4 +45,3 @@ for letter in  display:
      word+=letter
 
 
-print(word)
